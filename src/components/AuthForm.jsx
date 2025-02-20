22e74d5d509b40ead4e499d20ef354c03f52ab15
ick-web <yih000098@gmail.com>
2025-02-20 17:47:45 +0900

    design: css 스타일링

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -13,40 +13,49 @@ const AuthForm = ({ mode, onSubmit }) => {
   };
 
   const handleSubmit = (e) => {
-    setFormData(e)
+    setFormData(e);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="id"
-        value={formData.id}
-        onChange={handleChange}
-        placeholder="아이디"
-        required
-      />
-      <input
-        type="text"
-        name="password"
-        value={formData.password}
-        onChange={handleChange}
-        placeholder="비밀번호"
-        required
-      />
-      {mode === "signup" && (
+    <div>
+      <form onSubmit={handleSubmit} className="flex flex-col items-center ">
         <input
           type="text"
-          name="nickname"
-          value={formData.nickname}
+          name="id"
+          value={formData.id}
           onChange={handleChange}
-          placeholder="닉네임"
+          placeholder="아이디"
           required
-          className="w-full p-4 border border-gray-300 rounded-lg"
+          className="w-full p-4 m-4 border border-gray-300 rounded-lg"
         />
-      )}
-      <button type="submit">{mode === "login" ? "로그인" : "회원가입"}</button>
-    </form>
+        <input
+          type="text"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="비밀번호"
+          required
+          className="w-full p-4 m-4 border border-gray-300 rounded-lg"
+        />
+        {mode === "signup" && (
+          <input
+            type="text"
+            name="nickname"
+            value={formData.nickname}
+            onChange={handleChange}
+            placeholder="닉네임"
+            required
+            className="w-full p-4 m-4 border border-gray-300 rounded-lg"
+          />
+        )}
+        <button
+          type="submit"
+          className="bg-blue-500 text-white w-full hover:bg-blue-400 p-4 m-4 border-2 border-solid rounded-xl"
+        >
+          {mode === "login" ? "로그인" : "회원가입"}
+        </button>
+      </form>
+    </div>
   );
 };
 
