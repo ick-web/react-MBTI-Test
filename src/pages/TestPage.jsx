5027d649c69fcec41eb3fb980acd2b43b4d3e9d9
ick-web <yih000098@gmail.com>
2025-02-24 14:55:28 +0900

    feat: 테스트 결과 제출 시 테스트 결과 UI 구현

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -10,6 +10,7 @@ const TestPage = () => {
 
   const handleTestSubmit = async (answers) => {
     const mbtiResult = calculateMBTI(answers);
+    setResult(mbtiResult);
     /* Test 결과는 mbtiResult 라는 변수에 저장이 됩니다. 이 데이터를 어떻게 API 를 이용해 처리 할 지 고민해주세요. */
     try {
       return await createTestResult({ result: mbtiResult });
